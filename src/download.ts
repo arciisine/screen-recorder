b18ad8e5b01aa7af4c9a0d5d6c80d8d80c7c7227
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2021-06-20 12:07:52 -0400

    Fixing

--- src/download.ts
+++ src/download.ts
@@ -131,7 +131,7 @@ export class DownloadUtil {
 
     const zipFile = await new Promise<yauzl.ZipFile>(
       (resolve, reject) => yauzl.open(zipLoc, { lazyEntries: true },
-        (err, zip) => err ? reject(err) : resolve(zip)));
+        (err, zip) => err ? reject(err) : resolve(zip!)));
 
     const zipEntry = await new Promise<yauzl.Entry>(
       (resolve, reject) => {
@@ -150,7 +150,7 @@ export class DownloadUtil {
 
     const stream = await new Promise<NodeJS.ReadableStream>((resolve, reject) => {
       zipFile.openReadStream(zipEntry, (err, str) => {
-        err ? reject(err) : resolve(str);
+        err ? reject(err) : resolve(str!);
       });
     });
 
